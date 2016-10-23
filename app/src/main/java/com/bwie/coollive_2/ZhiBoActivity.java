e28c7e406bda61920103d443b51bb64262e2bb10
bmwh <783797352@qq.com>
2016-10-23 21:50:19 +0800

    Initial commit

---
+++ app/src/main/java/com/bwie/coollive_2/ZhiBoActivity.java
@@ -0,0 +1,13 @@
+package com.bwie.coollive_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ZhiBoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhi_bo);
+    }
+}
